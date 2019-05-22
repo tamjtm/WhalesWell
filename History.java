91807191f2f76d4d92d0abc6f3cdc58c86cef9df
tamjtm <45255184+tamjtm@users.noreply.github.com>
2019-05-22 21:23:27 +0700

    upload comment
    add History's and SuggestTool's comments

---
+++ History.java
@@ -0,0 +1,75 @@
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * History
+ *
+ *   This class represents an book purchaed history in Book Recommendation Engine.
+ *
+ *   Created by Nutaya Pravalphreukul (Pear)   ID 59070501032
+ *     9 May 2019
+ *
+ */
+public class History
+{
+    /** purchased book */
+    private Book purchasedBook;
+
+    /** time that user purchase book */
+    private Timestamp timestamp;
+
+    /**
+     *      Construct history instance and initialize all members in History Class for new history
+     *
+     * @param book  purchased book
+     */
+    public History(Book book)
+    {
+        purchasedBook = book;
+        timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    /**
+     *      get purchased book from history
+     *
+     * @return  purchased book
+     */
+    public Book getBook()
+    {
+        return purchasedBook;
+    }
+
+    /**
+     *      load purchased history and initialize book and timestamp member from kept data
+     * @param time  that user purchase book
+     * @param book  that user purchased
+     */
+    public void loadHistory(String time,Book book)
+    {
+        try 
+        {
+            this.purchasedBook = book;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+            Date parsedDate = dateFormat.parse(time);
+            this.timestamp = new java.sql.Timestamp(parsedDate.getTime());
+        }
+        catch(Exception e)
+        {
+            //this generic but you can control another types of exception
+            // look the origin of excption 
+        }
+    }
+
+    /**
+     *      used for display History Class information
+     *
+     * @return  string of History Class
+     */
+    public String toString()
+    {
+        return timestamp+"\t" +purchasedBook.getKeyword().get(0); 
+    }
+
+}
